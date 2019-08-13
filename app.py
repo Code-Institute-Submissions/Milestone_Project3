f54bbb7729832fc17caa83b2f5a85868175e4dc4
Ubuntu <ubuntu@ip-172-31-22-144.us-east-2.compute.internal>
2019-08-13 14:48:46 +0000

    add recipes page added - rendering of html pages issue fixed - good to continue with implementing the foundations

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
-
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
+
 mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addrecipe.html")
 
 
 if __name__ == '__main__':
