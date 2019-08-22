8cce6f1aadf979a709670b84ce57e14c1f53de65
Ubuntu <ubuntu@ip-172-31-22-144.us-east-2.compute.internal>
2019-08-22 21:29:35 +0000

    edit functionality passed - updated task cards are created when submitted

--- app.py
+++ app.py
@@ -13,21 +13,20 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 mongo = PyMongo(app)
 
 
+# recipe card display information 
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    data = []
-    with open('data/company.json', 'r') as json_data:
-        data = json.load(json_data)
     tasks=mongo.db.tasks.find()
-    return render_template("tasks.html", page_title="tasks.html", tasks=tasks, company=data) 
+    return render_template("tasks.html", page_title="tasks.html", tasks=tasks) 
 
-    
+
+# add recipe html page    
 @app.route('/add_recipe')
 def add_recipe():
     return render_template("addrecipe.html", page_title="addrecipe.html", categories=mongo.db.categories.find())
 
-    
+# insert recipe function from add_recipe.html to tasks.html  
 @app.route('/insert_recipe', methods=["GET", "POST"])
 def insert_recipe():
     tasks = mongo.db.tasks
@@ -35,21 +34,29 @@ def insert_recipe():
     return redirect(url_for('get_tasks'))
 
 
+# edit tasks.html info function
 @app.route('/edit_recipe/<task_id>')
 def edit_recipe(task_id):
     the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     all_categories = mongo.db.categories.find()
     return render_template("editrecipe.html", page_title="editrecipe.html", task=the_task, categories=all_categories)
-   
-"""
-@app.route('/edit_task/<task_id>')
-def edit_task(task_id):
-    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
-    all_categories =  mongo.db.categories.find()
-    return render_template('edittask.html', task=the_task,
-                           categories=all_categories)
-"""
+    
 
+# update function to save edited data
+@app.route('/confirm_task/<task_id>', methods=["POST"])
+def confirm_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update({'_id': ObjectId(task_id)},
+    {
+        'category_name':request.form.get('category_name'),
+        'task_name':request.form.get('task_name'),
+        'task_description':request.form.get('task_description'),
+        'task_note':request.form.get('task_note'),
+        'task_instructions':request.form.get('task_instructions')
+    })
+    return redirect(url_for('get_tasks'))
+    
+   
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
