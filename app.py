1d9d8869d0a1c504f5a9c5da33f1eb8bde3aa7d4
Ubuntu <ubuntu@ip-172-31-22-144.us-east-2.compute.internal>
2019-08-22 16:08:08 +0000

    edit_recipe html page loading corrctly - ready to add functionaility to edit tasks.html

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
+
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
@@ -25,13 +26,27 @@ def get_tasks():
 @app.route('/add_recipe')
 def add_recipe():
     return render_template("addrecipe.html", page_title="addrecipe.html", categories=mongo.db.categories.find())
-    
+
     
 @app.route('/insert_recipe', methods=["GET", "POST"])
 def insert_recipe():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
+
+
+@app.route('/edit_recipe')
+def edit_recipe():
+    return render_template("editrecipe.html", page_title="editrecipe.html", categories=mongo.db.categories.find())
+    
+"""
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task,
+                           categories=all_categories)
+"""
 
 
 if __name__ == '__main__':
